859f7a2e6e48c53d05918abfe47cc12c7028b9fd
Hozefa <hozefa24imp@gmail.com>
2023-08-23 17:06:17 +0530

    #Section7 71

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from accounts.forms import UserForm
+from accounts.models import User
 
 # Create your views here.
 def registerUser(request):
     # return HttpResponse('Test')
-    return render(request, 'accounts/registerUser.html')
+    if request.method == 'POST':
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm()
+
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/registerUser.html',context)
