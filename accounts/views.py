b2355199e00f0b5246369222765bec1c37b0953c
Hozefa <hozefa24imp@gmail.com>
2023-08-23 16:26:03 +0530

    #Section7 70

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse('Test')
+    # return HttpResponse('Test')
+    return render(request, 'accounts/registerUser.html')
