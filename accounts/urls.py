611a1498d2351a1af467e284510272804a45e253
Hozefa <hozefa24imp@gmail.com>
2023-08-28 18:39:06 +0530

    Section10 98

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('',views.myAccount),
     path('registerUser/',views.registerUser, name='registerUser'),
     path('registerVendor/',views.registerVendor, name='registerVendor'),
     path('login/',views.login, name='login'),
