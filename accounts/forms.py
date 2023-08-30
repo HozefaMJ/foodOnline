a143076c4df77554ea872675e888863a6aeea051
Hozefa <hozefa24imp@gmail.com>
2023-08-30 11:24:13 +0530

    Section11 107-108

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,6 +23,17 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing..','required':'required'}))
     class Meta:
         model = UserProfile
-        fields = ['profile_picture','cover_photo','address_line_1','address_line_2','country','state','city','pincode','latitude','longitude']
+        fields = ['profile_picture','cover_photo','address','country','state','city','pincode','latitude','longitude']
+    
+    # In Vast Scenarios use this solution:
+    def __init__(self,*args,**kwargs):
+        super(UserProfileForm, self).__init__(*args,**kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
+
