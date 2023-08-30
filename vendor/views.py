472572151f20dcad7e649828c5d19545c4bdbecb
Hozefa <hozefa24imp@gmail.com>
2023-08-30 17:41:04 +0530

    Section13 123

--- vendor/views.py
+++ vendor/views.py
@@ -49,7 +49,7 @@ def vprofile(request):
 @user_passes_test(check_role_vendor)
 def menu_builder(request):
     vendor = get_vendor(request)
-    categories = Category.objects.filter(vendor=vendor)
+    categories = Category.objects.filter(vendor=vendor).order_by('created_at')
     context = {
         'categories':categories
     }
@@ -85,3 +85,24 @@ def add_category(request):
         'form':form,
     }
     return render(request, 'vendor/add_category.html',context)
+
+def edit_category(request,pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(form.cleaned_data['category_name'])
+            form.save()
+            messages.success(request,'Category Updated!')
+            return redirect('menu_builder')
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        'form':form,
+        'category':category,
+    }
+    return render(request, 'vendor/edit_category.html',context)
