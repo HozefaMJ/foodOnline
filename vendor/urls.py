b7834e41ca9fc502b2ef8c69e87d788acec14002
Hozefa <hozefa24imp@gmail.com>
2023-08-31 11:54:03 +0530

    Section14 127

--- vendor/urls.py
+++ vendor/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
 
     #FoodItem CRUD
     path('menu-builder/food/add/',views.add_food,name='add_food'),
+    path('menu-builder/food/edit/<int:pk>',views.edit_food,name='edit_food'),
+    
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -67,6 +67,9 @@ def fooditems_by_category(request,pk=None):
     }
     return render(request, 'vendor/fooditems_by_category.html',context)
 
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -86,6 +89,9 @@ def add_category(request):
     }
     return render(request, 'vendor/add_category.html',context)
 
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def edit_category(request,pk=None):
     category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
@@ -107,12 +113,18 @@ def edit_category(request,pk=None):
     }
     return render(request, 'vendor/edit_category.html',context)
 
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def delete_category(request,pk=None):
     category = get_object_or_404(Category,pk=pk)
     category.delete()
     messages.error(request,'Category Deleted')
     return redirect('menu_builder')
 
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def add_food(request):
     if request.method == 'POST':
         form = FoodItemForm(request.POST,request.FILES)
@@ -132,3 +144,27 @@ def add_food(request):
         'form':form,
     }
     return render(request, 'vendor/add_food.html',context)
+
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def edit_food(request,pk=None):
+    food = get_object_or_404(FoodItem, pk=pk)
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES, instance=food)
+        if form.is_valid():
+            food = form.save(commit=False)
+            food.vendor = get_vendor(request)
+            food.slug = slugify(form.cleaned_data['food_title'])
+            form.save()
+            messages.success(request,'Food Updated!')
+            return redirect('fooditems_by_category',food.category.id)
+        else:
+            print(form.errors)
+    else:
+        form = FoodItemForm(instance=food)
+    context = {
+        'form':form,
+        'food':food,
+    }
+    return render(request, 'vendor/edit_food.html',context)
